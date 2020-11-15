1130a4724e63c2c3cdcfb112f670903d6e439a6a
AmbidexterT <stepan210699@gmail.com>
2020-11-15 16:43:44 +0300

    Update project

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "../elements/Header/Header";
+import NotFound from "../elements/NotFound/NotFound";
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <Header />
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/:movieId" component={Movie} exact />
+            <Route component={NotFound} />
+          </Switch>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
