5714fd43391fef7d628943324c20672d7354769a
STEPAN PETROV <stepan210699@gmail.com>
2020-12-14 17:45:41 +0300

    add synopsis

--- src/components/elements/MovieInfoBar/MovieInfoBar.js
+++ src/components/elements/MovieInfoBar/MovieInfoBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import FontAwesome from "react-fontawesome";
 
-import { calcTime, convertMoney } from "../../../helpers";
+import { calcTime } from "../../../helpers";
 import "./MovieInfoBar.css";
 
 const MovieInfoBar = props => {
@@ -14,18 +14,7 @@ const MovieInfoBar = props => {
             Running Time: {calcTime(props.time)}
           </span>
         </div>
-        <div className="rmdb-movieinfobar-content-col">
-          <FontAwesome className="fa-budget" name="money" size="2x" />
-          <span className="rmdb-movieinfobar-info">
-            Budget: {convertMoney(props.budget)}
-          </span>
-        </div>
-        <div className="rmdb-movieinfobar-content-col">
-          <FontAwesome className="fa-revenue" name="ticket" size="2x" />
-          <span className="rmdb-movieinfobar-info">
-            Revenue: {convertMoney(props.revenue)}
-          </span>
-        </div>
+    
       </div>
     </div>
   );
